12b1eb984ece02364ebbdb3b0458b257d7f799ab
Lance Rowan <40448980+netluo@users.noreply.github.com>
2023-11-30 16:29:08 +0800

    Add files via upload

---
+++ concurrency.go
@@ -0,0 +1,50 @@
+// Package concurrency coding=utf-8
+// @Project : concurrency
+// @Time    : 2023/11/24 16:28
+// @Author  : chengxiang.luo
+// @Email   : chengxiang.luo@pharmaron.com
+// @File    : concurrency.go
+// @Software: GoLand
+package go_toolkit
+
+import (
+	"sync"
+)
+
+type Semaphore struct {
+	c  chan struct{}
+	wg sync.WaitGroup
+}
+
+// NewSemaphore returns a new Semaphore initialized to the given value.
+func NewSemaphore(maxCount int) *Semaphore {
+	return &Semaphore{c: make(chan struct{}, maxCount)}
+}
+
+// Acquire acquires a permit, blocking until it becomes available or ctx is done.
+func (s *Semaphore) Acquire(delta int) {
+	s.wg.Add(delta)
+	for i := 0; i < delta; i++ {
+		s.c <- struct{}{}
+	}
+}
+
+// Release releases a permit.
+func (s *Semaphore) Release() {
+	<-s.c
+	s.wg.Done()
+}
+
+// Wait blocks until all permits have been released.
+func (s *Semaphore) Wait() {
+	s.wg.Wait()
+}
+
+// AcquireWithFunc gets the semaphore and executes the callback function with arguments
+func (s *Semaphore) AcquireWithFunc(f func(args ...interface{}), args ...interface{}) {
+	go func() {
+		defer s.Release()
+		s.Acquire(1)
+		f(args...)
+	}()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/netluo/go-toolkit
+
+go 1.20
